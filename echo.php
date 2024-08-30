f82d343355670084b624e79773641713414e6657
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2024-08-31 00:56:49 +0530

    Echo ke bare meh

---
+++ echo.php
@@ -0,0 +1,6 @@
+<?php 
+    echo "Echo is Case Insensitive"; echo "<br>" ;echo "Echo is used to Display the output";
+    echo "<br>";
+    echo "<br> tag is used for next line"; //br tag used for next line
+
+?>

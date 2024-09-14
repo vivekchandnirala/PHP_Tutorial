efeed8708fbca9168a56b944bece3c145653dafc
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2024-09-15 01:37:40 +0530

    Constant in PHP

---
+++ constant.php
@@ -0,0 +1,10 @@
+
+<?php 
+    const NAME = "Vivek"; //1st Type to Declare Constants
+    echo "My name is ",NAME;
+
+    echo "<br>";  // For Break the Line
+
+    define("AGE","21");
+    echo "My Age is ",AGE;
+?>

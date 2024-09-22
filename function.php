85a14ae00abde399527d5cba3ce38fd8c5ce404b
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2024-09-23 01:13:46 +0530

    Basic Function

---
+++ function.php
@@ -0,0 +1,17 @@
+<!-- Function in PHP  -->
+ <?php 
+ function user(){
+    heading(); // Calling the Function
+    echo "Hello User <br />";
+    echo "Welcome to New World <br />";
+    echo "This is My First PHP Function <br /><hr />";
+ }
+
+ function heading(){
+    echo "<h1>Welcome to My Website</h1>";
+ }
+
+ user(); // Calling the Function
+ user(); // Calling the Function
+ user(); // Calling the Function
+ ?>

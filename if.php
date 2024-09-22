591d686ceee438eabfc18e1deb2ff9797460c5cd
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2024-09-22 22:46:54 +0530

    If Condition

---
+++ if.php
@@ -0,0 +1,12 @@
+<!-- if condition in PHP -->
+ <?php 
+ $age = 17;
+ $name = "Vivek";
+ if($age<18){
+     echo "You are under 18 years old";
+ }
+ if($name == "Vivek"){
+    echo "<h1>Welcome $name Chand Nirala</h1>";
+ }
+
+ ?>

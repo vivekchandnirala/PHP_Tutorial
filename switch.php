3f0b561c868ee37cfd2e4693b10eb693d44e6fd4
Vivek Chand Nirala <vivekchandnirala1@gmail.com>
2024-09-22 23:14:52 +0530

    Switch Condition

---
+++ switch.php
@@ -0,0 +1,21 @@
+<!-- Switch Condition -->
+ <?php 
+ $color = "yellow";
+
+ switch($color){
+    case "red":
+        echo "Nehru House";
+        break;
+    case "blue":
+        echo "tagore House";
+        break;
+    case "yellow":
+        echo "Ashoka House";
+        break;
+    case "green":
+        echo "Raman House";
+        break;
+    default:
+        echo "Invalid color";
+ }
+ ?>
